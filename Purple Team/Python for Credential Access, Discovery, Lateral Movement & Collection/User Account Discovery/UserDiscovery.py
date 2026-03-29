5d7fe417fcb2fcea6a9ecdc34dede87ddc8e32e6
Pedro Guimarães <115589064+pedroguimaraes92@users.noreply.github.com>
2026-03-29 20:03:49 -0300

    Update UserDiscovery.py

--- Purple Team/Python for Credential Access, Discovery, Lateral Movement & Collection/User Account Discovery/UserDiscovery.py
+++ Purple Team/Python for Credential Access, Discovery, Lateral Movement & Collection/User Account Discovery/UserDiscovery.py
@@ -1,13 +1,17 @@
-
-import os,wmi
+import os
+import wmi
 
 w = wmi.WMI()
 
 # Get list of Administrator Accounts
-admins = None
+admins = []
 for group in w.Win32_Group():
     if group.Name == "Administrators":
-        admins = [a.Name for a in group.associators(wmi_result_class="Win32_UserAccount")]
+        admins = [
+            a.Name
+            for a in group.associators(wmi_result_class="Win32_UserAccount")
+        ]
+        break
 
 # List user accounts on device
 for user in w.Win32_UserAccount():
@@ -15,11 +19,11 @@ for user in w.Win32_UserAccount():
     print("Administrator: %s" % (user.Name in admins))
     print("Disabled: %s" % user.Disabled)
     print("Local: %s" % user.LocalAccount)
-    print("Password Changeable: %s"%user.PasswordChangeable)
+    print("Password Changeable: %s" % user.PasswordChangeable)
     print("Password Expires: %s" % user.PasswordExpires)
     print("Password Required: %s" % user.PasswordRequired)
     print("\n")
 
 # Print Windows Password Policy
 print("Password Policy:")
-print(os.system("net accounts"))
+os.system("net accounts")
